e41213a0d1e08578f8c9d187696404de95570f2d
Pushpa <31432288+PushpaKamatham@users.noreply.github.com>
2024-02-18 11:02:05 +0530

    cros changes has commited

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const connectDB = require('./db');
-const productRoutes = require('./routes/products');
+const express = require("express");
+const connectDB = require("./db");
+const cors = require("cors");
+const productRoutes = require("./routes/products");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Access-Control-Allow-Headers, Content-Type, Authorization, Origin, Accept",
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+const PORT = process.env.PORT || 3090;
 
 // Load environment variables
-require('dotenv').config();
+require("dotenv").config();
 
 // Connect to MongoDB
 connectDB();
 
 // Routes
-app.use('/products', productRoutes);
+app.use("/products", productRoutes);
+// Use the cors middleware to enable CORS
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
